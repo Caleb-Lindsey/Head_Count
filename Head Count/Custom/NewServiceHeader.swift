f35f12ce562cd6449298068ff8b9f4ec61e0b29c
Cal Lindsey <boldferociousdogwarrior@gmail.com>
2018-02-13 00:49:18 -0500

    commit

--- Head Count/Custom/NewServiceHeader.swift
+++ Head Count/Custom/NewServiceHeader.swift
@@ -10,6 +10,69 @@ import UIKit
 
 class NewServiceHeader : UIView {
     
+    var titleField : CustomTextField = {
+        let textField = CustomTextField()
+        textField.placeholder = "Event Title"
+        return textField
+    }()
     
+    var counterField : CustomTextField = {
+        let textField = CustomTextField()
+        textField.placeholder = "Name of Counter"
+        return textField
+    }()
+    
+    var locationField : CustomTextField = {
+        let textField = CustomTextField()
+        textField.placeholder = "Location of Event"
+        return textField
+    }()
+    
+    var dateButton : UIButton = {
+        let button = UIButton()
+        button.setTitle("1/21/18", for: .normal)
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = Global.grayColor
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        
+        // Place Title Field
+        titleField.frame = CGRect(x: 15, y: 15, width: self.frame.width - 30, height: self.frame.height / 5)
+        self.addSubview(titleField)
+        
+        // Place Counter Field
+        counterField.frame = CGRect(x: 15, y: titleField.frame.maxY + 5, width: titleField.frame.width, height: titleField.frame.height)
+        self.addSubview(counterField)
+        
+        // Place Location Field
+        locationField.frame = CGRect(x: 15, y: counterField.frame.maxY + 5, width: counterField.frame.width, height: counterField.frame.height)
+        self.addSubview(locationField)
+        
+    }
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Head Count/Views/NewServiceView.swift
+++ Head Count/Views/NewServiceView.swift
@@ -8,13 +8,67 @@
 
 import UIKit
 
-class NewServiceView : HeadCountVC {
+class NewServiceView : HeadCountVC, UITableViewDataSource, UITableViewDelegate {
+    
+    var cellID : String = "cellID"
+    
+    var newServiceTable : UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = UIColor.clear
+        return tableView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationItem.title = "New Event"
         
+        let newLayer = CAGradientLayer()
+        newLayer.colors = [Global.grayColor.cgColor, UIColor.white.cgColor]
+        newLayer.frame = view.frame
+        newLayer.frame.origin.y = (self.navigationController?.navigationBar.frame.maxY)!
+        self.view.layer.insertSublayer(newLayer, at: 0)
+        
+        // Place New Service Table
+        newServiceTable.frame = view.frame
+        newServiceTable.delegate = self
+        newServiceTable.dataSource = self
+        newServiceTable.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
+        view.addSubview(newServiceTable)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : UITableViewCell = newServiceTable.dequeueReusableCell(withIdentifier: cellID)!
+        cell.textLabel?.text = "hello"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 65
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return view.frame.height / 3
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let newServiceHeader : NewServiceHeader = NewServiceHeader()
+        return newServiceHeader
     }
     
 }
+
+
+
+
+
+
+
+
+
+
